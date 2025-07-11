d8bdb87b08360167208df89aefbd4efa282b1401
Alejandbel <bogdanov28c@gmail.com>
2025-07-11 20:49:05 +0300

    fix: fix any

--- src/utils/any-signal.ts
+++ src/utils/any-signal.ts
@@ -1,4 +1,27 @@
+// TODO: fixme
+function AbortSignalany(iterable: AbortSignal[]) {
+    const controller = new AbortController();
+    /**
+     * @this AbortSignal
+     */
+    function abort() {
+        // @ts-expect-error e
+        controller.abort(this.reason);
+        clean();
+    }
+    function clean() {
+        for (const signal of iterable) signal.removeEventListener('abort', abort);
+    }
+    for (const signal of iterable)
+        if (signal.aborted) {
+            controller.abort(signal.reason);
+            break;
+        } else signal.addEventListener('abort', abort);
+
+    return controller.signal;
+}
+
 export function anySignal(...signals: (AbortSignal | null | undefined)[]) {
     const existingSignals = signals.filter((signal) => signal !== null && signal !== undefined);
-    return AbortSignal.any(existingSignals);
+    return AbortSignalany(existingSignals);
 }
