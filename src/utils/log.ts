751e228b8097311c61bc42371724345e13cc4029
callmedenchick <d.volkov@top.team>
2025-08-11 15:23:39 +0200

    small fixes

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,20 +1,20 @@
 /* eslint-disable no-console */
 
 // Possible log levels: 'debug', 'warning', 'error'
-const LOG_LEVEL = process.env.BRIDGE_SDK_LOG_LEVEL || 'error';
+const LOG_LEVEL = process.env.TON_CONNECT_BRIDGE_SDK_LOG_LEVEL || 'error';
 
 export const logDebug: typeof console.debug = (...args) => {
     if (LOG_LEVEL === 'debug') {
-        console.debug('[BRIDGE_SDK_LOG]', ...args);
+        console.debug('[TON_CONNECT_BRIDGE_SDK]', ...args);
     }
 };
 
 export const logError: typeof console.error = (...args) => {
-    console.error('[BRIDGE_SDK_LOG]', ...args);
+    console.error('[TON_CONNECT_BRIDGE_SDK]', ...args);
 };
 
 export const logWarning: typeof console.warn = (...args) => {
     if (LOG_LEVEL === 'debug' || LOG_LEVEL === 'warning') {
-        console.warn('[BRIDGE_SDK_LOG]', ...args);
+        console.warn('[TON_CONNECT_BRIDGE_SDK]', ...args);
     }
 };
